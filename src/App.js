bcf027e0df4687a59b52bf43c8abc3d3731c5375
LAKSHITgoel <lakshit.goel@kuliza.com>
2019-02-03 12:53:02 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ export default class App extends Component {
       counter: 0
     };
   }
+
+  onClick = e => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
   render() {
     return (
       <div>
         <h1>Hello React </h1>
-        <h1 onClick={() => this.setState({ counter: this.state.counter + 1 })}>
-          {this.state.counter}
-        </h1>
+        <h1 onClick={this.onClick}>{this.state.counter}</h1>
       </div>
     );
   }
